785f9996b8939cd20143385a8e77821524cd4e59
yi xin <3077501050@qq.com>
2024-07-07 11:09:52 +0800

    实现自动加载 Windows App Runtime, 删除 WindowsAppSDKSelfContained

---
+++ FluentWinver/Program.cs
@@ -0,0 +1,23 @@
+﻿using WinRT;
+using Microsoft.UI.Xaml;
+using FluentWinver;
+
+namespace FluentWinverProg
+{
+    public static class Program
+    {
+        internal static void Main(string[] args)
+        {
+            if (!RuntimeHelper.IsMSIX)
+            {
+                if (!WARInitializer.InitializeWAR(out int hresult))
+                {
+                    System.Environment.Exit(hresult);
+                }
+            }
+            ComWrappersSupport.InitializeComWrappers();
+            Application.Start((p) => new App());
+            return;
+        }
+    }
+}
---
+++ FluentWinver/WARInitializer.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.Windows.ApplicationModel.DynamicDependency;
+using System;
+using System.IO;
+using System.Text.Json;
+
+internal static class WARInitializer
+{
+    public static bool InitializeWAR(out int HResult)
+    {
+        uint? majorMinorVersion = WASVersionInfoFromConfig.MajorMinorVersion;
+        PackageVersion? minVersion = WASVersionInfoFromConfig.MinVersion;
+        string versionTag = WASVersionInfoFromConfig.VersionTag;
+        if (majorMinorVersion.HasValue && minVersion.HasValue)
+        {
+            try
+            {
+                bool result = Bootstrap.TryInitialize(majorMinorVersion.Value, versionTag, minVersion.Value,
+                    NoMatchAndShowUIOption, out HResult);
+                return result;
+            }
+            catch (Exception ex)
+            {
+                HResult = ex.HResult;
+                return false;
+            }
+        }
+        else
+        {
+            HResult = 1;
+            return false;
+        }
+    }
+
+    public static Bootstrap.InitializeOptions NoMatchAndShowUIOption
+    {
+        get => Bootstrap.InitializeOptions.OnNoMatch_ShowUI;
+    }
+
+    public static class WASVersionInfoFromConfig
+    {
+        static JsonElement? verconfig;
+        static readonly JsonDocumentOptions JsonDocumentOptions = new()
+        {
+            AllowTrailingCommas = true,
+            CommentHandling = JsonCommentHandling.Skip,
+        };
+        static void GetConfig()
+        {
+            try
+            {
+                string configString = File.ReadAllText("WASDKVersionInfo.json");
+                JsonDocument configDoc = JsonDocument.Parse(configString, JsonDocumentOptions);
+                verconfig = configDoc.RootElement.GetProperty("WASDKVersionInfo");
+            }
+            catch (Exception)
+            {
+                verconfig = null;
+            }
+        }
+
+        public static uint? MajorMinorVersion
+        {
+            get
+            {
+                if (!verconfig.HasValue) GetConfig();
+                try
+                {
+                    if (verconfig.HasValue)
+                    {
+                        string rawdata = verconfig.Value.GetProperty("MajorMinorVersion").GetString();
+                        return Convert.ToUInt32(rawdata, 16);
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                }
+                catch (Exception)
+                {
+                    return null;
+                }
+            }
+        }
+
+        public static PackageVersion? MinVersion
+        {
+            get
+            {
+                if (!verconfig.HasValue) GetConfig();
+                try
+                {
+                    if (verconfig.HasValue)
+                    {
+                        string rawdata = verconfig.Value.GetProperty("MinVersion").GetString();
+                        return new PackageVersion(Convert.ToUInt64(rawdata, 16));
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                }
+                catch (Exception)
+                {
+                    return null;
+                }
+            }
+        }
+
+        public static string VersionTag
+        {
+            get
+            {
+                if (!verconfig.HasValue) GetConfig();
+                try
+                {
+                    if (verconfig.HasValue)
+                    {
+                        return verconfig.Value.GetProperty("VersionTag").GetString();
+                    }
+                    else
+                    {
+                        return "";
+                    }
+                }
+                catch (Exception)
+                {
+                    return "";
+                }
+            }
+        }
+    }
+}
