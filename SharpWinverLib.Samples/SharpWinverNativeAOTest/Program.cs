3a650d25a5ba0ae1abc4baf7c7f804d1f0a426c3
yi xin <3077501050@qq.com>
2024-11-06 23:18:37 +0800

    添加测试项目

---
+++ SharpWinverLib.Samples/SharpWinverNativeAOTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using SharpWinver;
+
+namespace SharpWinverNativeAOTest;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("-------------------------------");
+        Console.WriteLine("Test sample for SharpWinver Library on Native AOT");
+        Console.WriteLine("-------------------------------");
+        Console.WriteLine("----Current Windows OS Info----");
+        Console.WriteLine("-------------------------------");
+        Console.WriteLine("Windows Edition" + ":  " + WindowsEdition.OSEditionName);
+        Console.WriteLine("Windows Version" + ":  " + WindowsVersion.ReleaseVersionTag);
+        Console.WriteLine("OS Version" + ":  " + WindowsVersion.FullVersionTag);
+        Console.WriteLine("OS Architecture" + ":  " + WindowsEdition.OSArchitecture);
+        Console.WriteLine("-------------------------------");
+        Console.WriteLine("Press any key to continue...");
+        Console.ReadKey();
+    }
+}
