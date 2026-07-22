f6c3cf49ead6ad712581ab4b441966cc49f10d6c
MinecraftXinYi <3077501050@qq.com>
2026-07-22 23:08:23 +0800

    [v0.8.1]

---
+++ FluentWinver/Helpers/ExperiencePackInfoHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+using Windows.Management.Deployment;
+
+namespace FluentWinver.Helpers;
+
+public static class ExperiencePackInfoHelper
+{
+    public static readonly string WindowsCBSPackageFamilyName = "MicrosoftWindows.Client.CBS_cw5n1h2txyewy";
+
+    public static Package GetPackageForWindowsCBSPackage()
+        => packageManager.FindPackages(WindowsCBSPackageFamilyName).ToList()[default];
+
+    private static readonly PackageManager packageManager = new();
+}
